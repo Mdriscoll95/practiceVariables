59eca1176bed0ca227ceebdf17e16aee4a2d19ee
Mdriscoll95 <michael.driscoll951@gmail.com>
2023-05-12 16:10:52 -0700

    practice

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string myName = "Mike";
+        int myAge = 27;
+        bool hasCar = true;
+        double avgNum = 100.00;
+        decimal myMoney = 100.00m;
+        char middleInitial = 'a';
+
+        Console.WriteLine($"Hello, my name is {myName}, my age is {myAge}, do i have a car {hasCar}, my number is {avgNum}, my money is {myMoney}, my middle initial is {middleInitial}");
+    }
+}
